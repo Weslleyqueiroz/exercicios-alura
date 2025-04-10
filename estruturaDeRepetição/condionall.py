e18f75713d6a3d000cfe4d3e6e99ea670872c30f
Weslleyqueiroz <weslleyqueiroz773@gmail.com>
2025-04-09 21:13:45 -0300

    att dia 09/05

--- estruturaDeRepetição/condionall.py
+++ estruturaDeRepetição/condionall.py
@@ -8,7 +8,7 @@ triangulo = base + lado1 >lado2 and base + lado2 > lado1 and lado1 +lado2 > base
 if triangulo:
     print("É um triangulo")
 if base == lado1 ==lado2:
-    print("É um triangulo equilatero")
+    print("É um triangulo e quilatero")
 elif base == lado1 or base == lado2 or lado1==lado2:
     print("É isoceles")
 else:
---
+++ estruturaDeRepetição/enquanto.py
@@ -0,0 +1,45 @@
+#incrementação em py
+#cont = 1
+#while cont <=10:
+#    print(cont)
+#    cont = cont+1
+
+#for i in range(1,11):
+#   print(i)
+
+
+#1) Escreva um programa que peça dois números inteiros e imprima todos os números inteiros entre eles.
+#numero1 = int(input("Digite um número inteiro : "))
+#numero2 = int(input("Digite um número inteiro : "))
+#for i in range (numero1,numero2): 
+#   print(i)
+
+#coloniaA = 4
+#coloniaB = 10
+
+#taxa_A = 0.4
+#taxa_B = 0.10
+
+#dias = 0
+
+#while coloniaA< coloniaB:
+#    coloniaA += coloniaA * taxa_A
+#    coloniaB +=coloniaB * taxa_B
+#    dias = dias+1
+
+#    print(f"A taxa de crescimento será em {dias}")
+
+#cont = 0
+
+#while cont <= 15:
+#    nota = int(input("Digite uma nota de 0 a 5 : "))
+#    if nota >5:
+#        print("Nota valida")
+#        cont = cont+1
+#    else: 
+#        print("Nota invalida, tente novamente")
+
+#6) Escreva um programa que gere a tabuada 
+#de um número inteiro de 1 a 10, de acordo com 
+#a escolha da pessoa usuária. Como exemplo, para o 
+#número 2, a tabuada deve ser mostrada no seguinte formato:  
