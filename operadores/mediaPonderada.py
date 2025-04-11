faf3c42a344e78c31740ca431870fdc6c80cbcb5
Weslleyqueiroz <weslleyqueiroz773@gmail.com>
2025-04-11 14:18:07 -0300

    att

---
+++ operadores/mediaPonderada.py
@@ -0,0 +1,9 @@
+
+numero1 = 5  
+numero2 = 12
+numero3 = 20
+numero4 = 15
+mediaPonderada = (5*1)+(12*2)+(20*3)+(15*4)
+resutlado = mediaPonderada/10
+
+print(f'A media ponderada desse calculo é : {resutlado}')
