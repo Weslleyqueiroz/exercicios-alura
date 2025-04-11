a5b7daa507d1948cf5ed6f1978e78c925be4c4fc
Weslleyqueiroz <weslleyqueiroz773@gmail.com>
2025-04-11 14:51:06 -0300

    atualização exercicio consoante

--- estruturaDeRepetição/volgalConsoante.py
+++ estruturaDeRepetição/volgalConsoante.py
@@ -1,2 +1,9 @@
 #Escreva um programa que determine se uma letra fornecida pela pessoa usuária é uma vogal ou consoante.
 
+digite = str(input("Digite uma letra :"))
+
+
+if digite == 'a' or digite == 'e' or digite == 'i' or digite == 'o' or digite == 'u':
+    print(f"A letra que você digitou : {digite} é uma vogal")
+else:
+    print(f"A letra que você digitou : {digite} é uma consoante")
